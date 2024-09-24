0644dd02c9a356da2ac6d8014c0ab91a2cecf93b
jisoii <jisoii47@gmail.com>
2024-09-24 13:18:26 +0900

    [fix] : 삭제 메뉴 수정

--- src/Delete/DeleteWord.java
+++ src/Delete/DeleteWord.java
@@ -15,15 +15,29 @@ public class DeleteWord {
 
         for (int i = 0; i < wordList.size(); i++) {
             if (wordList.get(i).getEnglish().equalsIgnoreCase(searchWord)) {
-                wordList.remove(i);
-                found = true;
-                System.out.println("단어가 삭제되었습니다.");
+            	found = true;
+            	
+            	while(true) {
+            	System.out.println("\n'" + wordList.get(i).getEnglish() + "'을/를 단어장에서 삭제하시겠습니까?");
+                System.out.println("(1) 예");
+                System.out.println("(2) 아니오");
+                System.out.print("메뉴를 선택하시오 : ");
+                int confirmation = scanner.nextInt();
+                if (confirmation == 1) {
+                    wordList.remove(i);
+                    System.out.println("단어가 삭제되었습니다.\n");
+                    break;
+                } else if (confirmation == 2) {
+                    System.out.println("삭제가 취소되었습니다.\n");
+                    break;
+                } else System.out.println("숫자 1 또는 2를 입력해주세요.");
+            	}
                 break;
             }
         }
 
         if (!found) {
-            System.out.println("해당 단어가 존재하지 않습니다.");
+            System.out.println("해당 단어가 존재하지 않습니다.\n");
         }
     }
 }
