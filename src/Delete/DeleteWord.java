3a761b9e5398fb3b6b18a95f78b928b1ab61434d
jisoii <jisoii47@gmail.com>
2024-11-04 10:21:27 +0900

    [fix] : 삭제 확인 반복문 수정

--- src/Delete/DeleteWord.java
+++ src/Delete/DeleteWord.java
@@ -1,6 +1,7 @@
 package Delete;
 
 import App.Word;
+
 import java.util.List;
 import java.util.Scanner;
 
@@ -22,19 +23,23 @@ public class DeleteWord {
 
         if (wordToDelete != null) {
             // 삭제 확인 절차 추가
-            System.out.printf("‘%s : %s’을/를 단어장에서 삭제하시겠습니까?\n", wordToDelete.getEnglishWord(), wordToDelete.getMeanings().values().iterator().next());
-            System.out.println("(1) 예");
-            System.out.println("(2) 아니오");
-            System.out.print("메뉴를 선택하세요 >> ");
+            while (true) {
+                System.out.printf("‘%s : %s’을/를 단어장에서 삭제하시겠습니까?\n", wordToDelete.getEnglishWord(), wordToDelete.getMeanings().values().iterator().next());
+                System.out.println("(1) 예");
+                System.out.println("(2) 아니오");
+                System.out.print("메뉴를 선택하세요 >> ");
 
-            String choice = scanner.nextLine();
-            if (choice.equals("1")) {
-                wordList.remove(wordToDelete);
-                System.out.println("'" + english + "' 단어가 삭제되었습니다.\n");
-            } else if (choice.equals("2")) {
-                System.out.println("삭제가 취소되었습니다.\n");
-            } else {
-                System.out.println("오류: 숫자 1 또는 2를 입력해주세요.");
+                String choice = scanner.nextLine();
+                if (choice.equals("1")) {
+                    wordList.remove(wordToDelete);
+                    System.out.println("'" + english + "' 단어가 삭제되었습니다.\n");
+                    break;
+                } else if (choice.equals("2")) {
+                    System.out.println("삭제가 취소되었습니다.\n");
+                    break;
+                } else {
+                    System.out.println("오류: 숫자 1 또는 2를 입력해주세요.\n");
+                }
             }
         } else {
             System.out.println("단어장에 '" + english + "' 단어가 존재하지 않습니다.\n");
