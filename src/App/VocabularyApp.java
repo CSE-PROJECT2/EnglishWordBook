b7b2d4b43d9d772af0bbb0d4bfc8da165cab86a7
jiyeKa <jiyekim0304@gmail.com>
2024-11-30 10:13:09 +0900

    fix : 출력 형식 일치
    6.1 비정상 결과에 '숫자 외 문자 입력한 오류'에 "오류: 숫자를 입력해주세요." 처리 안 했으므로,
    모든 비정상 결과를 "잘못된 입력입니다. 다시 시도해주세요."으로 통일

--- src/App/VocabularyApp.java
+++ src/App/VocabularyApp.java
@@ -63,7 +63,7 @@ public class VocabularyApp {
                         System.out.println("잘못된 입력입니다. 다시 시도해주세요.");
                 }
             } catch (NumberFormatException e) {
-                System.out.println("오류: 숫자를 입력해주세요.");
+                System.out.println("잘못된 입력입니다. 다시 시도해주세요.");
             }
         }
     }
